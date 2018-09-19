89174515ae4348d9e81de1561c5512b2d554e89b
Ruslan Shakirov <shakirovrrr@outlook.com>
2018-09-19 16:22:36 +0300

    Ex2

--- week5/ex1.c
+++ week5/ex1.c
@@ -6,11 +6,21 @@
 void *threadJob(void *argp) {
 	printf("\tThread number %d\n", *(int *) argp);
 
+	pthread_exit(0);
 	return 0;
 }
 
 int main() {
 	pthread_t threads[NTHREADS];
+	printf("Parallel: \n");
+	for (int i = 1; i <= NTHREADS; ++i) {
+		printf("Thread %d start\n", i);
+		int iArg = i;
+		pthread_create(&threads[i], 0, threadJob, &iArg);
+		printf("Thread %d end\n", i);
+	}
+
+	printf("\nSequential: \n");
 	for (int i = 1; i <= NTHREADS; ++i) {
 		printf("Thread %d start\n", i);
 		int iArg = i;
