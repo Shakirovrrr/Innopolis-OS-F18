f2b021134bb856d60099dba441ac535a6a1438a9
Ruslan Shakirov <shakirovrrr@outlook.com>
2018-09-19 15:06:05 +0300

    Ex1

--- week5/ex1.c
+++ week5/ex1.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
+#include <pthread.h>
+
+#define NTHREADS 10
+
+void *threadJob(void *argp) {
+	printf("\tThread number %d\n", *(int *) argp);
+
+	return 0;
+}
 
 int main() {
-	printf("Hello, World!\n");
+	pthread_t threads[NTHREADS];
+	for (int i = 1; i <= NTHREADS; ++i) {
+		printf("Thread %d start\n", i);
+		int iArg = i;
+		pthread_create(&threads[i], 0, threadJob, &iArg);
+		pthread_join(threads[i], 0); // Fix here
+		printf("Thread %d end\n", i);
+	}
+
 	return 0;
 }
