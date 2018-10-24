aeefa4a42aba1dab7ee3474bbcc6257a81bba0bd
Ruslan Shakirov <shakirovrrr@outlook.com>
2018-10-24 15:30:14 +0300

    Ex2

--- week10/ex2.sh
+++ week10/ex2.sh
@@ -1,4 +1,5 @@
 FILEDIR='../week1/_file.txt'
+printf "Some contents of _file.txt\n" > $FILEDIR
 link $FILEDIR '_ex2.txt' > 'ex2.txt'
 INODENUM=$(ls -i $FILEDIR | awk '{print $1}')
 find ../ -inum $INODENUM >> 'ex2.txt'
