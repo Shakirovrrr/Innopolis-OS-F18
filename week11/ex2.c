efa07b47f21218f7f5b55b8b49a70b6fd0b38c61
Ruslan Shakirov <shakirovrrr@outlook.com>
2018-10-31 16:31:47 +0300

    Ex2

--- week11/ex2.c
+++ week11/ex2.c
@@ -1,4 +1,17 @@
-//
-// Created by Ruslan on 31-Oct-18.
-//
+#include <stdio.h>
+#include <unistd.h>
 
+int main(){
+	printf("H");
+	sleep(1);
+	printf("e");
+	sleep(1);
+	printf("l");
+	sleep(1);
+	printf("l");
+	sleep(1);
+	printf("o");
+	sleep(1);
+
+	return 0;
+}
